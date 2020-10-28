e5002914f2d7e32c189ed46741c1238459ccd9a1
nguyenthithuuyen <nuyen2054@gmail.com>
2020-10-28 12:24:17 +0700

    php

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+function bubbleSort($list)
+{
+    for ($i = 0; $i < count($list) - 1; $i++) {
+        for ($j = $i + 1; $j < count($list); $j++) {
+            if ($list[$i] > $list[$j]) {
+                $temp = $list[$j];
+                $list[$j] = $list[$i];
+                $list[$i] = $temp;
+            }
+        }
+    }
+    return $list;
+}
+$arr=[2,3,2,5,6,1,-2,3,14,12];
+echo "Mảng khi chưa sắp xếp:\n";
+echo implode(' , ',$arr );
+echo "<br>";
+echo "Mảng khi đã sắp xếp:\n";
+echo implode(' , ',bubbleSort($arr) );
